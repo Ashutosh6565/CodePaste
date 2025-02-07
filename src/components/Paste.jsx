278e6927eee1e0c6cb70ccb5e0075b3b1872f8aa
Ashutosh <kumarashutosh7302401472@gmail.com>
2025-02-08 02:45:48 +0530

    till here we have made edit delete copy view as well as other button next is the edit button in the code

--- src/components/Paste.jsx
+++ src/components/Paste.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { removeFromPastes } from '../redux/pasteSlice';
+import toast from 'react-hot-toast';
+import { Link } from 'react-router-dom';
 
 const Paste = () => {
+
+  const pastes = useSelector((state) => state.paste.pastes);
+  const [searchTerm, setSearchTerm] = useState('');
+  const dispatch = useDispatch();
+
+  const filteredData = pastes.filter(
+    (paste) => paste.title.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+
+  function handleDelete(pasteId){
+    dispatch(removeFromPastes(pasteId)); 
+  }
   return (
     <div>
-      paste
+      <input 
+      className='p-2 rounded-2xl min-w-[600px] mt-5'
+      type="search"
+      placeholder='search here'
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <div className='flex flex-col gap-5'>
+    {
+      filteredData.length > 0 && 
+      filteredData.map(
+        (paste) => {
+          return (
+            <div className='border' key ={ paste?._id}>
+              <div>
+                {paste.title}
+              </div>
+              <div>
+                {paste.content}
+              </div>
+              <div className='flex flex-row place-content-evenly gap-4'>
+                <button  >
+                  <Link to={`/?pasteId=${paste?._id}`}>Edit</Link>
+                </button>
+                <button>
+                 <Link to={`/pastes/${paste?._id}`}>View</Link>
+                </button>
+                  
+                
+                <button onClick={() =>handleDelete(paste?._id)} >
+                  Delete
+                </button>
+                <button onClick={() => {
+                  navigator.clipboard.writeText(paste?.content)
+                  toast.success("copied to clipboard")
+                }}>
+                  copy
+                </button>
+                <button >
+                  share
+                </button>
+              </div>
+              <div>
+                {paste.createdAt}
+              </div>
+              </div>
+          )
+        }
+      )
+    }
+      </div>
     </div>
   )
 }
--- src/redux/pasteSlice.js
+++ src/redux/pasteSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-
+import toast from 'react-hot-toast';
 
 const  initialState = {
   pastes:localStorage.getItem("pastes")
@@ -15,17 +15,32 @@ export const pasteSlice = createSlice({
    addToPastes: (state,action) =>{
     const paste = action.payload;
     state.pastes.push(paste);
-    localStorage.setItem("pastes",state.pastes);
-    
+    localStorage.setItem("pastes",JSON.stringify(state.pastes));
+    toast("paste created Succesfully")
    },
    UpdateToPastes: (state,action) => {
-
+    const paste = action.payload;
+    const index = state.pastes.findIndex((item) => item._id === paste._id);
+    if(index >= 0){
+        state.pastes[index] = paste;
+        localStorage.setItem("pastes" ,JSON.stringify(state.pastes));
+        toast.success("paste updated");
+    }
    },
    resetAllPastes: (state,action) => {
-
+    state.pastes = [],
+    localStorage.removeItem("pastes");
    },
    removeFromPastes: (state,action) => {
+    const pasteId = action.payload;
+    console.log(pasteId);
+    const index = state.pastes.findIndex((item) => item._id === pasteId);
+    if(index >= 0){
+        state.pastes.splice(index,1);
+        localStorage.setItem("pastes",JSON.stringify(state.pastes));
 
+        toast.success("paste deleted");
+    }
    },
   },
 })
